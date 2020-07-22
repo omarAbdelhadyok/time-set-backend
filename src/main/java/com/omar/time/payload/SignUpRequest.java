0ee9d94f1e4f292920d278c58699136caeb4ced7
Omar Abdelhady <omar.abdelhady.ok@gmail.com>
2020-07-22 20:00:01 +0300

    updated signup validation rules

--- src/main/java/com/omar/time/payload/SignUpRequest.java
+++ src/main/java/com/omar/time/payload/SignUpRequest.java
@@ -2,7 +2,6 @@ package com.omar.time.payload;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Getter;
@@ -12,19 +11,16 @@ import lombok.Setter;
 @Setter
 public class SignUpRequest {
 	
-    @NotBlank
-    @Size(min = 4, max = 40)
+    @Size(max = 40)
     private String firstName;
     
-    @NotBlank
-    @Size(min = 4, max = 40)
+    @Size(max = 40)
     private String lastName;
 
     @NotBlank
     @Size(min = 3, max = 15)
     private String username;
     
-    @NotBlank
     @Size(max = 20)
     private String mobile;
 
@@ -35,8 +31,6 @@ public class SignUpRequest {
 
     @NotBlank
     @Size(min = 8, max = 20)
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", 
-	message = "Should be at least 8 characters, at least one uppercase, one lowercase, one number, one special character")
     private String password;
 
 }
