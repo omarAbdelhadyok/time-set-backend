a603c2ac627684c6286ff519848a6403ec43801e
Omar Abdelhady <omar.abdelhady.ok@gmail.com>
2020-07-28 19:58:34 +0300

    no expiry for jwt token

--- src/main/java/com/omar/time/security/JwtTokenProvider.java
+++ src/main/java/com/omar/time/security/JwtTokenProvider.java
@@ -31,12 +31,8 @@ public class JwtTokenProvider {
 
         UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
 
-        Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
-
         return Jwts.builder().setSubject(Long.toString(userPrincipal.getId()))
-            .setIssuedAt(new Date()).setExpiration(expiryDate)
-            .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+            .setIssuedAt(new Date()).signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
     }
 
     public Long getUserIdFromJWT(String token) {
