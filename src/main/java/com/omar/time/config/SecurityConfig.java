4aea979dd5d4f688cff5a398f60b2bdf6b6a19ca
Omar Abdelhady <omar.abdelhady.ok@gmail.com>
2020-11-02 22:08:39 +0300

    updated spring-fox to version 3.0.0 to support @NotBlank and @NotEmpty

--- src/main/java/com/omar/time/config/SecurityConfig.java
+++ src/main/java/com/omar/time/config/SecurityConfig.java
@@ -102,8 +102,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(AUTH_WHITELIST);
     }
-
-    
     
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
